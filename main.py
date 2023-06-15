2e80e04db2fa2be3e78bbadb8bad21fbaf6eaf63
tomasdopacio <tomasdopacio1@gmail.com>
2023-06-15 15:06:49 -0300

    Contraseña Encriptada

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv , requests
 
+from passlib.context import CryptContext
+
 #validaciones 
 def validation_yes_no(respuesta)->bool:
     return respuesta not in ["s" , "n"]
@@ -13,7 +15,13 @@ def validation_mail(mail, lista_de_mails)->bool:
 def validation_equipos(equipo_elegido, equipos_existentes)->bool:
     return equipo_elegido not in equipos_existentes
 
+#------------------------MENU----------------------------------------------------------------------------------------------------
+def menu()->None:
+    print("Usted inicio sesion exitosamente")
+    buscar_jugadores_por_equipo()
+#------------------------MENU----------------------------------------------------------------------------------------------------
 
+#1------------------------------------------------------------------------------------------------------------------------------
 #lee el archivo csv y lo convierte en una lista                
 def lista_informacion_de_usuarios()->list:
     lista_info = []
@@ -26,7 +34,6 @@ def lista_informacion_de_usuarios()->list:
 
     return lista_info
 
-#1------------------------------------------------------------------------------------------------------------------------------
 #lee archivos csv y lo convierte en un diccionario 
 
 def diccionario_infromacion_usuarios()->dict:
@@ -41,9 +48,6 @@ def diccionario_infromacion_usuarios()->dict:
     return diccionario_info
 
 #Aca va el programa y todas sus funciones
-
-def menu()->None:
-    print("usted a ingresado al menu o sea ingreso sesion exitosamente")
 
 def iniciar_sesion()-> None:
 
@@ -68,22 +72,45 @@ def iniciar_sesion()-> None:
            
             print("intentemoslo denuevo")
 
-    password = input("Ingrese la contraseña")
-    while (password != dic_ingresado[mail]["password"]):
-            print("contraseña incorrecta")
-            password = input("Ingrese de nuevo la contraseña, si quiere salir ingrese (SALIR) en mayusculas ")
-            if password == "SALIR":
-                break #SACAR
+    password = input("Ingrese la contraseña ")
 
-    if password == dic_ingresado[mail]["password"]:
+    password_cryp = dic_ingresado[mail]["password"]
+
+    while (True != verificar_contraseña(password,password_cryp)):
+            
+            print("contraseña incorrecta")
+            password = input("Ingrese de nuevo la contraseña ")
+            password_crypt = encriptar_contraseña(password)
+
+    if True == verificar_contraseña(password,password_cryp):
         menu()        
 
+def verificar_contraseña(password:str,password_crypt:str)->bool:
+    #Pre:Ingreso una variable con la contraseña y otra con la contraseña encryptada
+    #Post:Devuelve true si la contraseña correspone a la encriptacion
+    contexto = CryptContext(
+          schemes =["pbkdf2_sha256"],
+          default ="pbkdf2_sha256",
+          pbkdf2_sha256__default_rounds = 3000)
+    
+    return contexto.verify(password,password_crypt)
+
+def encriptar_contraseña(password:str)->str:
+    #Pre: Ingrese un str 
+    #Post: Devuelve el str encriptado
+    contexto = CryptContext(
+    schemes =["pbkdf2_sha256"],
+    default ="pbkdf2_sha256",
+    pbkdf2_sha256__default_rounds = 3000)
+
+    password_crypt = contexto.hash(password)
+
+    return password_crypt
 
 def creacion_usuario()-> None:   
     dic_ingresado = diccionario_infromacion_usuarios()
     list_ingresada = lista_informacion_de_usuarios()
     
-
     print("Bienvenido a #Jugarsela# ingrese sus datos para continuar")
     #ingreso del mail
     mail = input("Ingrese su mail: ")
@@ -96,19 +123,21 @@ def creacion_usuario()-> None:
 
     #ingreso de password
     password = input("Ingrese su contraseña: ")
+
+    #Encripto la password
     
+    password_crypt = encriptar_contraseña(password)
+
     print("Su informacion fue ingresada con exito")
 
     #Ingreso de usuario a la lista para despues que se suba al usuarios.csv
-    nuevo_usuario = [mail, username, password, 0,0,0]
+    nuevo_usuario = [mail, username, password_crypt, 0,0,0]
     list_ingresada.append(nuevo_usuario)
     
     #Sube la lista anterior con los usuarios ya existentes mas la nueva con el nuevo usuario al archivo usuarios.csv   
     with open("usuarios.csv", 'w', newline ='') as archivo_csv:
         writer = csv.writer(archivo_csv, delimiter=",")       
         writer.writerows(list_ingresada)
-
-
 
 def inicio()->None:
     qst = input("¿Es un usuario nuevo? s/n: ")
@@ -124,6 +153,8 @@ def inicio()->None:
 
 #2-----------------------------------------------------------------------------------------------
 def buscar_jugadores_por_equipo()->None:
+    #Pre:Ingreso un equipo(str) y busca mediante la api
+    #Post:Devuelve un print del plantel del equipo correspondiente
     print("EQUIPOS EXISTENTES")
     ids_de_equipos = impresion_equipos_liga_profesional()
     print()
@@ -140,7 +171,7 @@ def buscar_jugadores_por_equipo()->None:
 
     url = "https://v3.football.api-sports.io/players"
 
-    params={
+    params={#Parametros para filtrar los endpoint
         "league":"128",
         "season":"2023",
         "team" : id_de_equipo
@@ -157,9 +188,9 @@ def buscar_jugadores_por_equipo()->None:
     for i in range(len(reponse_json["response"])):     
        print("-", reponse_json["response"][i]["player"]["name"])
     
-
 def impresion_equipos_liga_profesional()->dict:
-    
+    #Pre: No ingreso nada
+    #Post: Genera un diccionario con ids y los equipos, y tambien genera un print con los equipos de la liga utilizando la api
     url = "https://v3.football.api-sports.io/teams?country=Argentina&league=128&season=2023"
 
     payload={}
@@ -182,7 +213,7 @@ def impresion_equipos_liga_profesional()->dict:
 #2-------------------------------------------------------------------------------------------------------------------------
 def main() -> None:
     inicio()
-    buscar_jugadores_por_equipo()
+    
 
       
 main()
